22da99bdacf43d7eb8c4c51f3504148c0e4e61e3
nathaniaelvina <nathaniaelvina@hotmail.com>
2017-11-22 17:23:22 +0700

    setup

---
+++ Monegement/TransactionItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monegement
+{
+    class TransactionItem
+    {
+        public string type { get; set; }
+        public string category { get; set; }
+        public string nominal { get; set; }
+        public string notes { get; set; }
+        public string transaction_date { get; set; }
+        public string id_user { get; set; }
+    }
+}
---
+++ Monegement/transactionPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Monegement
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class transactionPage : Page
+    {
+        public transactionPage()
+        {
+            this.InitializeComponent();
+            ObservableCollection<TransactionItem> dataList = new ObservableCollection<TransactionItem>();
+            TransactionItem t1 = new TransactionItem() { type = "Income", category = "Entertainment", nominal = "30000", notes = "test food", transaction_date = "23-11-2017", id_user = "001" };
+            TransactionItem t2 = new TransactionItem() { type = "Income", category = "Entertainment", nominal = "30000", notes = "test food", transaction_date = "23-11-2017", id_user = "001" };
+            TransactionItem t3 = new TransactionItem() { type = "Income", category = "Entertainment", nominal = "30000", notes = "test food", transaction_date = "23-11-2017", id_user = "001" };
+
+            dataList.Add(t1);
+            dataList.Add(t2);
+            dataList.Add(t3);
+            lvTransaction.ItemsSource = dataList;
+        }
+        private async void btnAddIncome_Click(object sender, RoutedEventArgs e)
+        {
+
+            AddIncomeDialog addIncome = new AddIncomeDialog();
+            await addIncome.ShowAsync();
+
+        }
+
+        private async void btnAddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            AddExpenseDialog addExpense = new AddExpenseDialog();
+            await addExpense.ShowAsync();
+
+        }
+    }
+}
